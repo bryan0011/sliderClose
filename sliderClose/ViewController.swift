6210bf98218ea649a10261e4979bf9f1df5a5ab0
Bryan Kuo <67792469+bryan0011@users.noreply.github.com>
2021-06-25 09:20:08 +0800

    Initial Commit

---
+++ sliderClose/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sliderClose
+//
+//  Created by Bryan Kuo on 2021/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
